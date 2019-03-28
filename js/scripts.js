214aed56ed54e454c5fbc7571ce80b53946d10a7
Josh Hood <joshood@users.noreply.github.com>
2019-03-28 16:44:26 -0400

    Add smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,32 @@
 jQuery(document).ready(function() {
 
     /*
-        WOW
+        Initialize wow.js for animations
     */
     new WOW().init();
 
-});
+    /*
+        Smooth scrolling from navbar and footer links
+    */
+    $(".navbar a, .footer a").on('click', function(event) {
+        // Verify there is a hash value on the link (href="#*")
+        if(this.hash !== "") {
+            // console.log(this.hash);
+
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store the hash value
+            var hash = this.hash;
+
+            // jQuery animate to smoothly scroll
+            // 900 is the time in milliseconds it takes to scroll
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function(){
+                // Restore hash in URL (default click behavior)
+                window.location.hash = hash;
+            });
+        } // end if
+    });
+})
